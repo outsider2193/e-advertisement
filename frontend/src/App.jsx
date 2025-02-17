80ed7230d63acfd9c5060f646bb44f49da3b914d
Yogesh K Mudaliar <yogeshmudaliar2004@gmail.com>
2025-02-17 09:22:31 +0530

    Made Navbar page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,19 @@ import Registration from './components/Registration'
 import Login from './components/Login';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Navbar } from './components/Navbar';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
+        <Navbar></Navbar>
 
         <Routes>
           <Route path='/register' element={<Registration />}></Route>
           <Route path='/login' element={<Login />}></Route>
+
         </Routes>
         <ToastContainer />
       </div >
