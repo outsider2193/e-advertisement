d741d3357c3b903ce960ccabb45a6d1c109bef7f
Yogesh K Mudaliar <yogeshmudaliar2004@gmail.com>
2025-03-13 07:57:19 +0530

    Did Some changes

--- Backend/routes/adFetchingRoutes.js
+++ Backend/routes/adFetchingRoutes.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken, authorizedRoles } = require("../middleware/authMiddleware");
-const { getAds, getAdsByCityId, getAdsbyAreaId } = require("../controllers/fetchAds");
+const { getAds, getAdsByCityId } = require("../controllers/fetchAds");
 
 router.get("/ads", verifyToken, authorizedRoles("viewer", "advertiser", "admin"), getAds);
 router.get("/ads/city/:cityId", verifyToken, authorizedRoles("advertiser"), getAdsByCityId);
-router.get("/ads/area/:areaId", verifyToken, authorizedRoles("advertiser"), getAdsbyAreaId)
+
 module.exports = router;
